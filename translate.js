fa76506a92a04360833ebefa1b3758f81b9e924e
unLiShed <64497060+unLiShed@users.noreply.github.com>
2020-06-08 10:35:03 -0400

    Add files via upload

---
+++ translate.js
@@ -0,0 +1,35 @@
+
+            var translatableElements = [];
+            var translations = [];
+            var clickers = [];
+            
+            var addSet = function(translatableElementId, translation, clickerId) {
+                translatableElements.push(translatableElementId);
+                translations.push(translation);
+                clickers.push(clickerId);
+            };
+            
+            var eventListenerNum = 0;
+
+            var getTranslator = function(link, translation) {
+                return function() {
+                    link.textContent = translation;
+                }
+            };
+            
+            while(eventListenerNum < links.length) {
+                var curClicker = clickers[eventListenerNum];
+                var curLink = links[eventListenerNum];
+                var curTranslation = translations[eventListenerNum];
+                var curTranslator = getTranslator(curLink, curTranslation);
+                curClicker.addEventListener("click", curTranslator);
+                eventListenerNum++;
+            }
+            
+            if(links.length !== translations.length 
+                or links.length !== clickers.length
+                or translations.length !== clickers.length) {
+                
+                document.body.textContent = "Oh noes! Your arrays (links, translations, and clickers) have different lengths!"
+            
+            }
