1be9550bfbdbce82c813e23770881b023292d129
unLiShed <64497060+unLiShed@users.noreply.github.com>
2020-06-09 06:37:29 -0400

    A js library used to change textContent.
    This js library is incomplete, so do not use! Its intended use is to let you change the textContent of a webpage element in response to the user clicking a different element. To use, call the "addSet" function, with the first parameter being a string that is the ID of the element that will be translated (I will call this element "element 1"), parameter 2 being a string which will be element 1's future textContent, and the third parameter being the ID of the element that the user will click. You may call this function as many times as you like, and once you have called it as much as you want, call the function "addTranslators" to add event listeners. Warning: This is an incomplete js library with extremely limited functionality and this functionality possibly will not work as expected.

--- translate.js
+++ translate.js
@@ -1,35 +1,31 @@
+var translatableElements = [];
+var translations = [];
+var clickers = [];
+            
+var addSet = function(translatableElementId, translation, clickerId) {
+            translatableElements.push(translatableElementId);
+            translations.push(translation);
+            clickers.push(clickerId);
+};
+            
+var eventListenerNum = 0;
 
-            var translatableElements = [];
-            var translations = [];
-            var clickers = [];
-            
-            var addSet = function(translatableElementId, translation, clickerId) {
-                translatableElements.push(translatableElementId);
-                translations.push(translation);
-                clickers.push(clickerId);
-            };
-            
-            var eventListenerNum = 0;
-
-            var getTranslator = function(link, translation) {
-                return function() {
-                    link.textContent = translation;
-                }
-            };
-            
+var getTranslator = function(link, translation) {
+            return function() {
+                        link.textContent = translation;
+            }
+};
+var addTranslators = function() {
             while(eventListenerNum < links.length) {
-                var curClicker = clickers[eventListenerNum];
-                var curLink = links[eventListenerNum];
-                var curTranslation = translations[eventListenerNum];
-                var curTranslator = getTranslator(curLink, curTranslation);
-                curClicker.addEventListener("click", curTranslator);
-                eventListenerNum++;
+                        var curClicker = clickers[eventListenerNum];
+                        var curLink = links[eventListenerNum];
+                        var curTranslation = translations[eventListenerNum];
+                        var curTranslator = getTranslator(curLink, curTranslation);
+                        curClicker.addEventListener("click", curTranslator);
+                        eventListenerNum++;
             }
+};
             
-            if(links.length !== translations.length 
-                or links.length !== clickers.length
-                or translations.length !== clickers.length) {
-                
-                document.body.textContent = "Oh noes! Your arrays (links, translations, and clickers) have different lengths!"
-            
-            }
+if(links.length !== translations.length or links.length !== clickers.lengthor translations.length !== clickers.length) {
+            document.body.textContent = "Oh noes! Your arrays (links, translations, and clickers) have different lengths!"
+}
